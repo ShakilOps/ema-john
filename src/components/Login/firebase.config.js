38a88acb0ccab2b90292075bfbcfc1574147b92d
Shakil <shehzan.shakil98@gmail.com>
2021-03-15 05:17:35 +0600

    yeah

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB4mCN9xCXf86tzdJMwLyuPYg3h9v-OmLo",
+    authDomain: "ema-john-jhonkar.firebaseapp.com",
+    projectId: "ema-john-jhonkar",
+    storageBucket: "ema-john-jhonkar.appspot.com",
+    messagingSenderId: "790094734655",
+    appId: "1:790094734655:web:9551272d06afc5ee5b9f8a"
+  };
+
+  export default firebaseConfig;
